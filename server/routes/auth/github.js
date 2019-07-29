b34de58f8864a9b961ad66fae913e1c078599ecd
Thomas O'Brien <tbobrien612@gmail.com>
2019-07-29 19:27:52 +0200

    Consolidate passport.use

--- server/routes/auth/github.js
+++ server/routes/auth/github.js
@@ -5,68 +5,43 @@ const { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } = require('../../../secrets')
 const { User } = require('../../db')
 module.exports = router
 
-router.get(
-  '/',
-  function(req, res, next) {
-    if (!req.user || !req.user._id) return res.status(400).send('No user found')
-    const callbackURL =
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: GITHUB_CLIENT_ID,
+      clientSecret: GITHUB_CLIENT_SECRET
+    },
+    function(accessToken, refreshToken, profile, cb) {
+      cb(null, profile, { accessToken, refreshToken })
+    }
+  )
+)
+
+router.get('/', function(req, res, next) {
+  if (!req.user || !req.user._id) return res.status(400).send('No user found')
+
+  passport.authenticate('github', {
+    callbackURL:
       'http://localhost:3001/api/v0/auth/github/callback?_id=' + req.user._id
+  })(req, res, next)
+})
 
-    passport.use(
-      new GitHubStrategy(
-        {
-          clientID: GITHUB_CLIENT_ID,
-          clientSecret: GITHUB_CLIENT_SECRET,
-          callbackURL
-        },
-        function(accessToken, refreshToken, profile, cb) {
-          cb(null, profile, { accessToken, refreshToken })
-        }
-      )
-    )
+router.get('/callback', function(req, res, next) {
+  passport.authenticate('github', null, function(err, profile, tokens) {
+    if (!req.query._id) return res.send('No user in request')
 
-    next()
-  },
-  passport.authenticate('github', {
-    successRedirect: '/success',
-    failureRedirect: '/failure'
-  })
-)
+    User.findOne({ _id: req.query._id }, (err, user) => {
+      if (err) res.status(500).send(err)
+      if (!user) res.status(404).send()
 
-router.get(
-  '/callback',
-  function(req, res, next) {
-    passport.use(
-      new GitHubStrategy(
-        {
-          clientID: GITHUB_CLIENT_ID,
-          clientSecret: GITHUB_CLIENT_SECRET,
-          callbackURL: 'http://localhost:3001/api/v0/auth/github/callback'
-        },
-        function(accessToken, refreshToken, profile, cb) {
-          if (!req.query._id) return res.send('No user in request')
+      user.apps.github = {
+        profile,
+        username: profile.username,
+        accessToken: tokens.accessToken
+      }
 
-          User.findOne({ _id: req.query._id }, (err, user) => {
-            if (err) res.status(500).send(err)
-            if (!user) res.status(404).send()
-
-            user.apps.github = {
-              profile,
-              username: profile.username,
-              accessToken
-            }
-
-            user.save()
-            cb(null, profile, { accessToken, refreshToken })
-          })
-        }
-      )
-    )
-
-    next()
-  },
-  passport.authenticate('github', {
-    successRedirect: '/success',
-    failureRedirect: '/failure'
-  })
-)
+      user.save()
+      res.redirect('/')
+    })
+  })(req, res, next)
+})
